2eedafe1cfc49f65bfea54661c1df1072b889190
tobin <tobinchilongojr@gmail.com>
2022-01-15 20:11:37 +0200

    Add interactive to JS/Fix linter errors

--- src/todolist.js
+++ src/todolist.js
@@ -21,14 +21,14 @@ displayTasks() {
     const Done = task.completed ? check : uncheck;
     const Line = task.completed ? lineThrough : '';
     const taskTemp = `
-        <li class="item d-flex">
-          <div class="task d-flex">
-            <i class="fa ${Done} update-status" job="complete" data="${task.index}"></i>
-            <p class="ptag ${Line}" contenteditable="true"  data="${task.index}">${task.description}</p>
-          </div>
-          <i class="fa fa-trash-o delete-task" job="delete"  data="${task.index}"></i>
-        </li>
-        `;
+      <li class="item d-flex">
+        <div class="task d-flex">
+          <i class="fa ${Done} update-status" job="complete" data="${task.index}"></i>
+          <p class="ptag ${Line}" contenteditable="true"  data="${task.index}">${task.description}</p>
+        </div>
+        <i class="fa fa-trash-o delete-task" job="delete"  data="${task.index}"></i>
+      </li>
+      `;
     taskSection.innerHTML += taskTemp;
   });
   this.attachEvents();
